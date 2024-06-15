60de689811fc0090b1f7d3bb23fb7fd753119164
Vlad P <vlad@pysarenko.com>
2024-06-15 05:11:22 +0200

    upd

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,17 +1,11 @@
 import SimpleLightBox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
-
-console.log(galleryItems);
 
 const galleryRootEl = document.querySelector('.gallery');
 const galleryHtmlMarkup = galleryItems.map(createImageCardMarkup).join('');
 
-galleryRootEl.insertAdjacentHTML('afterbegin',
-    galleryHtmlMarkup);
-// galleryRootEl.addEventListener('click', onGalleryClick);
+galleryRootEl.insertAdjacentHTML('afterbegin', galleryHtmlMarkup);
 
 function createImageCardMarkup({ preview, original, description } = {}) {
   return `
@@ -27,78 +21,12 @@ function createImageCardMarkup({ preview, original, description } = {}) {
 			</li>`;
 }
 
-// function onGalleryClick(e) {
-//   e.preventDefault();
-//   console.log(e.target.dataset.source);
-//   gallery.open(e.target.dataset.source);
-// }
-
-// function openModalImage(src) {
-//   //   if (gallery.visible()) {
-//   // gallery.close();
-//   //   }
-
-//   gallery = basicLightbox.create(
-//     `<img src="${src}" width="1280" alt="original">`,
-//     {
-//       onClose: () => {
-//         window.removeEventListener('keydown', onKeyboardClick);
-//       },
-//     }
-//   );
-//   //   gallery.show();
-//   window.addEventListener('keydown', onKeyboardClick);
-// }
-
-// function closeModal() {
-//   //   gallery.close();
-// }
-
-// function onKeyboardClick(e) {
-//   const oldSrcIndex = findCurrentSrcIndex();
-//   // gallery.element().querySelector('img').src
-
-//   switch (e?.code) {
-//     case 'Escape':
-//       closeModal();
-//       return;
-
-//     case 'ArrowLeft':
-//     case 'ArrowDown':
-//       openModalImage(
-//         galleryItems[
-//           oldSrcIndex === 0 ? galleryItems.length - 1 : oldSrcIndex - 1
-//         ].original
-//       );
-//       break;
-
-//     case 'ArrowRight':
-//     case 'ArrowUp':
-//     case 'Space':
-//       openModalImage(
-//         galleryItems[
-//           oldSrcIndex === galleryItems.length - 1 ? 0 : oldSrcIndex + 1
-//         ].original
-//       );
-//       break;
-//   }
-// }
-
-// function findCurrentSrcIndex(src) {
-//   for (let i = 0; i < galleryItems.length; i += 1) {
-//     if (galleryItems[i].original === src) return i;
-//   }
-//   return;
-// }
-
 (function () {
-    let $gallery = new SimpleLightBox(
-        '.gallery a',
-        {
-            captionDelay: 1250,
-            captionType: 'attr',
-            captionsData: 'alt',
-            captionPosition: 'bottom',
-            overlayOpacity: 0.8,
-        });
+  let $gallery = new SimpleLightBox('.gallery a', {
+    captionDelay: 1250,
+    captionType: 'attr',
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    overlayOpacity: 0.8,
+  });
 })();
